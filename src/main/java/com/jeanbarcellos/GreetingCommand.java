b5eca40c61ffe4e271675ae5c0ba77ca4eacec1e
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-06-28 22:12:28 -0300

    First commit

---
+++ src/main/java/com/jeanbarcellos/GreetingCommand.java
@@ -0,0 +1,20 @@
+package com.jeanbarcellos;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "greeting", mixinStandardHelpOptions = true)
+public class GreetingCommand implements Runnable {
+
+    @Parameters(paramLabel = "<name>", defaultValue = "picocli", description = "Your name.")
+    String name;
+
+    @Override
+    public void run() {
+        System.out.printf("Hello %s, go go commando!\n", name);
+    }
+
+}
+
+// mvn clean install -U
+// mvn dependency:resolve -U
